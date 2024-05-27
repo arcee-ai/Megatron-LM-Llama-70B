e963d9ad8aefe74467d6c92b4a5d0c261535f384
Shamane Siri <gayal.sha92@gmail.com>
2024-05-27 17:09:22 +1200

    add workers to the pre-processing code

--- submit-preprocess.sh
+++ submit-preprocess.sh
@@ -1,8 +1,8 @@
 python3 tools/preprocess_data.py \
-        --input /workspace/gpt2/oscar-1GB.jsonl \
-        --output-prefix  /workspace/Luke-Shamane-processing/tokenized_data/shamane-luke \
-        --hf-tokenizer-path /workspace/Luke-Shamane-processing/tokenizer/base-llama3-70B  \
+        --input /workspace/megatron-lm/SEC-DATA/final_output.jsonl \
+        --output-prefix   /workspace/megatron-lm/SEC-DATA/sec-tokenized \
+        --hf-tokenizer-path /workspace/megatron-lm/huggingface-files/tokenizers/llama3_70B_base/models--meta-llama--Meta-Llama-3-70B/snapshots/b4d08b7db49d488da3ac49adf25a6b9ac01ae338  \
         --tokenizer-type Llama3Tokenizer \
         --extra-hf-tokens 256 \
-        --workers 1 \
+        --workers 24 \
         --append-eod
